fc3079f5c22326a11f6fdda85e5ae7d97679aece
RamyaaaU <Ramyashree.U_c@reyesccb.com>
2024-04-25 15:32:29 +0530

    seed villa table data to db

--- RavishingVilla.Web/RavishingVilla.Infrastructure/Migrations/20240425095954_SeedVillaToDB.Designer.cs
+++ RavishingVilla.Web/RavishingVilla.Infrastructure/Migrations/20240425095954_SeedVillaToDB.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RavishingVilla.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using RavishingVilla.Infrastructure.Data;
 namespace RavishingVilla.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240425095954_SeedVillaToDB")]
+    partial class SeedVillaToDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
