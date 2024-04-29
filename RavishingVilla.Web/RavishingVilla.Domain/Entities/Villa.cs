1340423b1b5a7cea558c38e07fb4940cd8b00584
RamyaaaU <Ramyashree.U_c@reyesccb.com>
2024-04-29 16:05:59 +0530

    added data annotation

--- RavishingVilla.Web/RavishingVilla.Domain/Entities/Villa.cs
+++ RavishingVilla.Web/RavishingVilla.Domain/Entities/Villa.cs
@@ -1,13 +1,17 @@
-﻿namespace RavishingVilla.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RavishingVilla.Domain.Entities
 {
     public class Villa
     {
         public int Id { get; set; }
         public required string Name { get; set; }
         public string? Description { get; set; }
+        [Display(Name = "Price per night")]
         public double Price { get; set; }
         public int Sqft { get; set; }
         public int Occupancy { get; set; }
+        [Display(Name="Image Url")]
         public string? ImageUrl { get; set; }
         public DateTime? CreatedDate { get; set; }
         public DateTime? UpdatedDate { get; set; }
