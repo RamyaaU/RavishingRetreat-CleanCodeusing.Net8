3c71b1aba6acff71a825adc9aa7d97ff67a764a3
RamyaaaU <Ramyashree.U_c@reyesccb.com>
2024-05-07 14:42:45 +0530

    implementation of villa number list and view

---
+++ RavishingVilla.Web/RavishingVilla.Web/Controllers/VillaNumberController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RavishingVilla.Domain.Entities;
+using RavishingVilla.Infrastructure.Data;
+
+namespace RavishingVilla.Web.Controllers
+{
+    public class VillaNumberController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VillaNumberController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var villaNumbers = _context.VillaNumbers.ToList();
+            return View(villaNumbers);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa villa)
+        {
+            if (villa.Name == villa.Description)
+            {
+                //name is teh field and it is case insensitive 
+                ModelState.AddModelError("name", "The description cannot exactly match the Name field");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Villas.Add(villa);
+                _context.SaveChanges();
+                TempData["success"] = "The villa has been created successfully";
+                return RedirectToAction("Index", "Villa");
+            }
+            return View(villa);
+        }
+
+        public IActionResult Update(int villaId)
+        {
+            Villa? obj = _context.Villas.FirstOrDefault(x => x.Id == villaId);
+            if (obj == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            if (ModelState.IsValid && obj.Id > 0)
+            {
+                _context.Villas.Update(obj);
+                _context.SaveChanges();
+                TempData["success"] = "The villa has been updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int villaId)
+        {
+            Villa? obj = _context.Villas.FirstOrDefault(x => x.Id == villaId);
+            if (obj is null)
+            { 
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa obj)
+        {
+            Villa? objFromDb = _context.Villas.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb is not null)
+            {
+                _context.Villas.Remove(objFromDb);
+                _context.SaveChanges();
+                TempData["success"] = "The villa has been deleted successfully.";
+                return RedirectToAction("Index");
+            }
+            TempData["error"] = "The villa could not be deleted";
+            return View();
+        }
+    }
+}
