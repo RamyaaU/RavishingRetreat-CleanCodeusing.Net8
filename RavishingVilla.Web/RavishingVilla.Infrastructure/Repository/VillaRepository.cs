3651209fb4dff9e972fe37dc63f6ad99a4d3865c
RamyaaaU <Ramyashree.U_c@reyesccb.com>
2024-05-15 12:57:28 +0530

    adding villa repository class

--- RavishingVilla.Web/RavishingVilla.Infrastructure/Repository/VillaRepository.cs
+++ RavishingVilla.Web/RavishingVilla.Infrastructure/Repository/VillaRepository.cs
@@ -1,39 +1,75 @@
-﻿using RavishingVilla.Application.Common.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using RavishingVilla.Application.Common.Interfaces;
 using RavishingVilla.Domain.Entities;
+using RavishingVilla.Infrastructure.Data;
 using System.Linq.Expressions;
 
 namespace RavishingVilla.Infrastructure.Repository
 {
     public class VillaRepository : IVillaRepository
     {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public VillaRepository(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
         public void Add(Villa entity)
         {
-            throw new NotImplementedException();
+            _applicationDbContext.Add(entity);
         }
 
         public void Delete(Villa entity)
         {
-            throw new NotImplementedException();
+            _applicationDbContext.Remove(entity);
         }
 
         public IEnumerable<Villa> GetAllVillas(Expression<Func<Villa, bool>>? filter = null, string? includeProperties = null)
         {
-            throw new NotImplementedException();
+            IQueryable<Villa> query = _applicationDbContext.Set<Villa>();
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+
+            return query.ToList();
         }
 
-        public IEnumerable<Villa> GetVilla(Expression<Func<Villa, bool>> filter, string? includeProperties = null)
+        public Villa GetVilla(Expression<Func<Villa, bool>> filter, string? includeProperties = null)
         {
-            throw new NotImplementedException();
+            IQueryable<Villa> query = _applicationDbContext.Set<Villa>();
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+
+            return query.FirstOrDefault();
         }
 
         public void Save()
         {
-            throw new NotImplementedException();
+            _applicationDbContext.SaveChanges();
         }
 
         public void Update(Villa entity)
         {
-            throw new NotImplementedException();
+            _applicationDbContext.Villas.Update(entity);
         }
     }
 }
